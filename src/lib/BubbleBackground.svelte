08439735079d4c69e7da50c36a6314ac187670eb
Thompson Bear <thompsonbear21@gmail.com>
2024-11-11 22:21:15 -0500

    bubble

---
+++ src/lib/BubbleBackground.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	const rows = 10;
+	const cols = 15;
+</script>
+
+<div class="absolute bottom-0 right-1/2 flex justify-center">
+	<div
+		class="relative grid min-h-screen w-[2200px] translate-x-[1100px] place-items-center"
+		style="grid-template-rows: repeat({rows}, minmax(0, 1fr)); grid-template-columns: repeat({cols}, minmax(0, 1fr));"
+	>
+		{#each Array(rows) as _, r}
+			{#each Array(cols) as _, c}
+				{#if c < cols / 2}
+					<div
+						class="grid place-items-center rounded-full bg-orange-500"
+						style="height: {r}rem; width: {r}rem; opacity: {((c + 1) / (cols / 2) - 0.2) *
+							(r / 7)};"
+					></div>
+				{:else}
+					<div
+						class="grid place-items-center rounded-full bg-orange-500"
+						style="height: {r}rem; width: {r}rem; opacity: {((cols - c) / (cols / 2) - 0.2) *
+							(r / 7)};"
+					></div>
+				{/if}
+			{/each}
+		{/each}
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import BubbleBackground from '$lib/BubbleBackground.svelte';
 </script>
 
 {@render children()}
+<BubbleBackground />
