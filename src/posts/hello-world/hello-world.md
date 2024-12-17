71c96785eadec23b9b0e9ae7e19cd47b1fc71b3f
Thompson Bear <thompsonbear21@gmail.com>
2024-12-16 19:44:51 -0500

    Restructured posts directory for organization

---
+++ src/posts/hello-world/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+description: Introduction and Sandbox.
+published: '12-16-2024'
+categories:
+  - fun
+visible: true
+---
+
+## Hello World
--- src/routes/app-api/posts/+server.ts
+++ src/routes/app-api/posts/+server.ts
@@ -4,13 +4,19 @@ import type { Post } from '$lib/types';
 async function getPosts() {
 	let posts: Post[] = [];
 
-	const paths = import.meta.glob('/src/posts/*.md', { eager: true });
+	const paths = import.meta.glob('/src/posts/*/*.md', { eager: true });
 
 	for (const path in paths) {
 		const file = paths[path];
-		const slug = path.split('/').at(-1)?.replace('.md', '');
+		const slug = path.split('/').at(-2);
 
-		if (file && typeof file === 'object' && 'metadata' in file && slug) {
+		if (
+			file &&
+			typeof file === 'object' &&
+			'metadata' in file &&
+			slug &&
+			path.endsWith(`${slug}.md`) // Directory Name must match File Name
+		) {
 			const metadata = file.metadata as Omit<Post, 'slug'>;
 			const post = { ...metadata, slug } satisfies Post;
 			post.visible && posts.push(post);
